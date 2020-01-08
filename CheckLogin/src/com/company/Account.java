4be97c168d40c394d18ee9038aa034bbb8b38b0f
Marian Puscasu <puscasumarian99@gmail.com>
2020-01-08 18:01:22 +0200

    Small file updates

---
+++ CheckLogin/src/com/company/Account.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Account {
+    private String user;
+    private String pass;
+
+    public Account(String user, String pass) {
+        this.user = user;
+        this.pass = pass;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+}
